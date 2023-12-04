4a103246c6965cec5a5113a27e0e8f55f9e2c93c
emmabieg <emma.biegemann@haw-hamburg.de>
2023-12-04 16:15:13 +0100

    -m "RS-9"
    database add new renter

--- app/src/main/java/com/example/renovi/viewmodel/RenovationViewModel.java
+++ app/src/main/java/com/example/renovi/viewmodel/RenovationViewModel.java
@@ -2,31 +2,8 @@ package com.example.renovi.viewmodel;
 
 import android.content.Context;
 
-import com.example.renovi.model.RenovationDataSource;
-
 public class RenovationViewModel {
-
-    private RenovationDataSource dataSource;
-
-    public RenovationViewModel(Context context) {
-        dataSource = new RenovationDataSource(context);
-    }
-
-    public void open() {
-        dataSource.open();
-    }
-
-    public void close() {
-        dataSource.close();
-    }
-
-    public long addMieter(String firstName, String lastName) {
-        return dataSource.addMieter(firstName, lastName);
-    }
-
-    public long addRenovation(String object, String advantages, String disadvantages,
-                              int cost, String paragraph, long mieterId) {
-        return dataSource.addRenovation(object, advantages, disadvantages, cost, paragraph, mieterId);
+    public RenovationViewModel() {
     }
 }
 
