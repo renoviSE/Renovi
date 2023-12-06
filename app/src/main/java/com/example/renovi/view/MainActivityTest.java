9b73fcd4f9a560ee90156206b1d0b5c5632e4138
Bamboo <75183449+Bamboo92@users.noreply.github.com>
2023-12-06 01:50:21 +0100

    Bugfix and new Activity
    - Main (Test) Activity
    - Refector

---
+++ app/src/main/java/com/example/renovi/view/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.renovi.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.renovi.R;
+
+public class MainActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+    }
+}
