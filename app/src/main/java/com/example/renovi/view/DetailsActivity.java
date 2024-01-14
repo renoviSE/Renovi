e5e0b1aa367e85ea795da3e9cad9eae0496f3dee
Cengofat <cengiz1710@hotmail.de>
2024-01-14 17:18:10 +0100

    RS-13 anfang

--- app/src/main/java/com/example/renovi/view/DetailsActivity.java
+++ app/src/main/java/com/example/renovi/view/DetailsActivity.java
@@ -13,9 +13,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.renovi.R;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DetailsActivity extends AppCompatActivity {
+    private String id;
+    private String kosten;
+    private String nachteile;
+    private String object;
+    private String vorteile;
+    private String paragraph;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +35,7 @@ public class DetailsActivity extends AppCompatActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        //@Todo bekommt naoch kein Intent. intent muss von der MainActivity übergeben werden als String
+        //@Todo bekommt noch kein Intent. intent muss von der MainActivity übergeben werden als String
         //@Todo ...damit man hier in der detailsactivit, nach dem string in der datenbank suchen und die daten anzeigen kann
         Intent intent = getIntent();
         String geplanteRenovierung = intent.getStringExtra(MainActivityTest.geplanteRenovierung);
@@ -39,9 +48,32 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     //@Todo datenbank muss geklärt werden
-    //private void getRenovierungsDaten(FirebaseFirestore db){
-    //    db.collection("Renovierung").whereEqualTo("object")
-    //}
+    private void getRenovierungsDaten(FirebaseFirestore db,String geplanteRenovierung){
+        db.collection("Renovierung").whereEqualTo("id",geplanteRenovierung).get().addOnCompleteListener(
+                characteristics ->{
+                    if (characteristics.isSuccessful()){
+                        for (DocumentSnapshot document : characteristics.getResult()) {
+                            if(document.exists()){
+                                setKosten(document.getString("kosten"));
+
+                                TextView kostenTextview = findViewById(new Integer("kosten"));
+                                kostenTextview.setText(kosten);
+
+                                //String kosten = document.getString("kosten");
+                                //String nachteile = document.getString(("nachteile"));
+                                //String object = document.getString("object");
+                                //String paragraph = document.getString("paragraph");
+                                //String vorteile = document.getString("vorteile");
+
+
+
+
+                            }
+                        }
+                    }
+
+                });
+    }
     private void initializeBackToMainButton() {
         Button startButton = findViewById(R.id.InboxToMainButton);
         startButton.setOnClickListener(view -> switchToMain());
@@ -69,5 +101,29 @@ public class DetailsActivity extends AppCompatActivity {
         } catch (ActivityNotFoundException e) {
             Toast.makeText(this, "Keine Anwendung gefunden, um diese URL zu öffnen", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setKosten(String kosten) {
+        this.kosten = kosten;
+    }
+
+    public void setNachteile(String nachteile) {
+        this.nachteile = nachteile;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    public void setVorteile(String vorteile) {
+        this.vorteile = vorteile;
+    }
+
+    public void setParagraph(String paragraph) {
+        this.paragraph = paragraph;
     }
 }
