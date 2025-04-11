4035699a59b32878ad2fc7bb817a3cfd4595eade
akaiiri <115646743+akaiiri@users.noreply.github.com>
2025-04-11 22:32:03 +0200

    Fixed Chat crash

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,6 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
